9f9c5047cd1ce18beea4718a33bdd334e07cd8ad
KseniyaBuslaeva <ksenyabuslaeva@mail.ru>
2022-12-22 15:56:32 +0300

    homework 09

--- HomeWork/09_2.py
+++ HomeWork/09_2.py
@@ -11,19 +11,21 @@ class Category:
     индекс вхождения новой категории в списке. Если такая категория уже есть, вызвать ошибку
     ValueError"""
 
-    def add(self, category: str) -> int:
-        if category in self.categories:
+    @classmethod
+    def add(cls, category: str) -> int:
+        if category in cls.categories:
             raise ValueError('element is already in list')
         else:
-            self.categories.append(category)
-            return self.categories.index(category)
+            cls.categories.append(category)
+            return cls.categories.index(category)
 
     """Написать метод класса get принимающий индекс и возвращающий категорию из списка
     категорий на этом индексе, если нет элемента на таком индексе, вызвать атрибут ValueError"""
 
-    def get(self, index: int) -> str:
+    @classmethod
+    def get(cls, index: int) -> str:
         try:
-            return self.categories[index]
+            return cls.categories[index]
         except:
             raise ValueError('index is outbound list')
 
@@ -31,22 +33,24 @@ class Category:
     удаляющий элемент из списка категорий на этом индексе, если нет элемента на таком
     индексе, ничего не делать, метод ничего возвращать не должен"""
 
-    def delete(self, index: int):
-        if index < len(self.categories):
-            self.categories.pop(index)
+    @classmethod
+    def delete(cls, index: int):
+        if index < len(cls.categories):
+            cls.categories.pop(index)
 
     """Написать метод update принимающий индекс категорий и новое название категории, если
     нет элемента на таком индексе, то новая категория должна добавляться с учетом того, что
     имена категорий уникальны, если новое имя категории нарушает уникальность в списке
     категорий, вызвать исключение ValueError"""
 
-    def update(self, index: int, category_new_name: str):
-        if index < len(self.categories) and self.categories.count(category_new_name) < 1:
-            self.categories[index] = category_new_name
-        elif self.categories.count(category_new_name) > 0:
+    @classmethod
+    def update(cls, index: int, category_new_name: str):
+        if index < len(cls.categories) and cls.categories.count(category_new_name) < 1:
+            cls.categories[index] = category_new_name
+        elif cls.categories.count(category_new_name) > 0:
             raise ValueError('category already in list')
-        elif index < len(self.categories):
-            self.categories.append(category_new_name)
+        elif index < len(cls.categories):
+            cls.categories.append(category_new_name)
 
 
 cat1 = Category()
--- HomeWork/09_3.py
+++ HomeWork/09_3.py
@@ -8,37 +8,42 @@ class Category:
         {'name': 'thriller', 'is_published': True},
         {'name': 'romantic', 'is_published': False}]
 
-    def add(self, category: dict) -> int:
-        if category in self.categories:
+    @classmethod
+    def add(cls, category: dict) -> int:
+        if category in cls.categories:
             raise ValueError('element is already in list')
         else:
-            self.categories.append(category)
-            return self.categories.index(category)
+            cls.categories.append(category)
+            return cls.categories.index(category)
 
-    def get(self, index: int) -> dict:
+    @classmethod
+    def get(cls, index: int) -> dict:
         try:
-            return self.categories[index]
+            return cls.categories[index]
         except:
             raise ValueError('index is outbound list')
 
-    def delete(self, index: int):
-        if index < len(self.categories):
-            self.categories.pop(index)
+    @classmethod
+    def delete(cls, index: int):
+        if index < len(cls.categories):
+            cls.categories.pop(index)
 
-    def update(self, index: int, category_new_name: dict):
-        if index < len(self.categories) and len(list(filter(lambda item: item['name'] == category_new_name['name'], self.categories))) < 1:
-            self.categories[index] = category_new_name
-        elif self.categories.count(category_new_name) > 0:
+    @classmethod
+    def update(cls, index: int, category_new_name: dict):
+        if index < len(cls.categories) and len(list(filter(lambda item: item['name'] == category_new_name['name'], cls.categories))) < 1:
+            cls.categories[index] = category_new_name
+        elif cls.categories.count(category_new_name) > 0:
             raise ValueError('category already in list')
-        elif index < len(self.categories):
-            self.categories.append(category_new_name)
+        elif index < len(cls.categories):
+            cls.categories.append(category_new_name)
 
     """Добавить метод make_published принимающий индекс категории и меняющий значение
     ключа is_published на True, если такого индекса нет, вызвать исключение ValueError"""
 
-    def make_published(self, index: int):
-        if index < len(self.categories):
-            self.categories[index].update({'is_published': True})
+    @classmethod
+    def make_published(cls, index: int):
+        if index < len(cls.categories):
+            cls.categories[index].update({'is_published': True})
         else:
             raise ValueError('index is outbound categories list')
 
@@ -46,9 +51,10 @@ class Category:
     значение ключа is_published на False, если такого индекса нет, вызвать исключение
     ValueError"""
 
-    def make_unpublished(self, index: int):
-        if index < len(self.categories):
-            self.categories[index].update({'is_published': False})
+    @classmethod
+    def make_unpublished(cls, index: int):
+        if index < len(cls.categories):
+            cls.categories[index].update({'is_published': False})
         else:
             raise ValueError('index is outbound categories list')
 
