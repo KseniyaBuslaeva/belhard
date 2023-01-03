d31da81888e7fef611fb15a4d632e642c9ccc446
KseniyaBuslaeva <ksenyabuslaeva@mail.ru>
2023-01-03 12:07:30 +0300

    Homework 10

--- HomeWork/10_3.py
+++ HomeWork/10_3.py
@@ -3,8 +3,23 @@
 with open('10_2.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
 
+#marks = '''!()-[]{};?@#$%:'"\,./^&amp;*_'''
+words = 0
+letters = 0
+
 for line in lines:
     if line.isspace():
         lines.pop(lines.index(line))
+    else:
+        sentence_words = line.split()
+        words = len(sentence_words)
+
+        for word in sentence_words:
+            letters += len(word)
+
+        print('words: ', words)
+        print('letters: ', letters)
+        words = 0
+        letters = 0
 
 print(lines)
